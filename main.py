28cb13e8f18926785e47fed7cc3a8170667facef
Cédric Laudrel <dek@iono.me>
2017-11-09 01:29:21 +0100

    values printing for testing is now working

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import onewire
 import ds18x20
 import bme280
 
+# sensor declarations
+bme280_enabled = True
+ds18b20_sensors = {
+        "inside0": bytearray(b'(\xf6\xd2\xa1\x04\x00\x00\xbb')
+        }
+
 # pin values : modify according to your wiring
 # for most esp8266 boards, you may refer to :
 # https://github.com/esp8266/Arduino/issues/584#issuecomment-123715951
@@ -18,3 +24,12 @@ bme = bme280.BME280(i2c=i2c)
 
 # DS18B20 sensor initialization
 ds = ds18x20.DS18X20(onewire.OneWire(machine.Pin(onewire_data)))
+
+while True:
+    print(bme.values)
+    ds.convert_temp()
+    for name, sensor in ds18b20_sensors.items():
+        temp = ds.read_temp(sensor)
+        print("{} : {}".format(name, temp))
+    time.sleep(10)
+
