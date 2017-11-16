ff1a5b9b344d8e2c67ae49edd4397ab6cf40c297
Cédric Laudrel <dek@iono.me>
2017-11-16 23:08:06 +0100

    add post_influxdb() procedure

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import onewire
 import ds18x20
 import bme280
 
+hostname = "esp8266"
+
 # influxdb database settings
 influxdb_protocol = 'http'
 influxdb_host = '192.168.20.26'
@@ -31,11 +33,20 @@ bme = bme280.BME280(i2c=i2c)
 # DS18B20 sensor initialization
 ds = ds18x20.DS18X20(onewire.OneWire(machine.Pin(onewire_data)))
 
-while True:
-    print(bme.values)
-    ds.convert_temp()
-    for name, sensor in ds18b20_sensors.items():
-        temp = ds.read_temp(sensor)
-        print("{} : {}".format(name, temp))
-    time.sleep(10)
+def post_influxdb(sensor, value):
+    url = "{}://{}:{}/write?db={}".format(influxdb_protocol, influxdb_host, influxdb_port, influxdb_db)
+    data = "{},host={} value={}".format(sensor, hostname, value)
+    resp = urequests.post(url, data=data)
+    if resp.status_code == 204:
+        return True
+    else:
+        return False
+
+#while True:
+#    print(bme.values)
+#    ds.convert_temp()
+#    for name, sensor in ds18b20_sensors.items():
+#        temp = ds.read_temp(sensor)
+#        print("{} : {}".format(name, temp))
+#    time.sleep(10)
 
