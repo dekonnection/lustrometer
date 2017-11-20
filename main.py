386390e8e0a1f81844882b803de1d4b7b481bbd4
Cédric 'deK' Laudrel <cedric.laudrel@nbs-system.com>
2017-11-20 13:44:31 +0100

    try mode 5 (max oversampling) for bme280

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import configuration
 
 # BME280 sensor initialization
 i2c = machine.I2C(scl=machine.Pin(configuration.bme280_pin_scl), sda=machine.Pin(configuration.bme280_pin_sda))
-bme = bme280.BME280(i2c=i2c)
+bme = bme280.BME280(mode=5, i2c=i2c)
 
 # DS18B20 sensor initialization
 ds = ds18x20.DS18X20(onewire.OneWire(machine.Pin(configuration.ds18b20_pin_data)))
